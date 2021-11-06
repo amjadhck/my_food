410c0c1c714fe9eb04f921c75ba78fab20d2d64b
amjadhck <amjadhck@gmail.com>
2021-11-06 16:13:53 +0530

    bottom tab bar added experiment with theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:my_food/screens/categories_screen.dart';
 import 'package:my_food/screens/category_meals.dart';
 import 'package:my_food/screens/meal_details.dart';
+import 'package:my_food/screens/tabs_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,15 +18,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Food',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.pink,
-          secondary: Colors.blue,
-        ),
+        //colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.black),
+        primaryColor: Colors.pink,
+        primarySwatch: Colors.pink,
+
+        //secondary: Colors.black,
+        //background: Colors.pink.shade500,
+        //onPrimary: Colors.red,
+        //onSecondary: Colors.amber,
+        //onBackground: Colors.blueGrey,
         canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
               bodyText1: const TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
+                color: Colors.white,
+                // color: Color.fromRGBO(20, 51, 51, 1),
               ),
               bodyText2: const TextStyle(
                 color: Color.fromRGBO(20, 51, 51, 1),
@@ -39,10 +46,38 @@ class MyApp extends StatelessWidget {
       ),
       //home: CategoriesScreen(),
       routes: {
-        '/': (context) => CategoriesScreen(),
+        '/': (context) => const TabsScreen(),
         CategoryMealsScreen.id: (context) => CategoryMealsScreen(),
         MealDetailScreen.id: (context) => MealDetailScreen(),
       },
     );
   }
 }
+
+      // theme: ThemeData(
+      //   colorScheme:
+      //       ColorScheme.fromSwatch().copyWith(
+      //     primary: Colors.pink,
+      //     secondary: Colors.black,
+      //     background: Colors.pink.shade500,
+      //     //onPrimary: Colors.red,
+      //     onSecondary: Colors.amber,
+      //     onBackground: Colors.blueGrey,
+      //   ),
+      //   canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+      //   fontFamily: 'Raleway',
+      //   textTheme: ThemeData.light().textTheme.copyWith(
+      //         bodyText1: const TextStyle(
+      //           color: Colors.white,
+      //           // color: Color.fromRGBO(20, 51, 51, 1),
+      //         ),
+      //         bodyText2: const TextStyle(
+      //           color: Color.fromRGBO(20, 51, 51, 1),
+      //         ),
+      //         caption: const TextStyle(
+      //           fontSize: 23,
+      //           fontFamily: 'RobotoCondensed',
+      //           fontWeight: FontWeight.bold,
+      //         ),
+      //       ),
+      // ),
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_final_fields, unused_field
+
+import 'package:flutter/material.dart';
+import 'package:my_food/screens/categories_screen.dart';
+import 'package:my_food/screens/favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoriteScree(), 'title': 'Your Favorites'},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Colors.amber,
+        currentIndex: _selectedPageIndex,
+        //type: BottomNavigationBarType.shifting,
+        mouseCursor: MouseCursor.defer,
+        selectedFontSize: 15,
+        unselectedFontSize: 10,
+        showUnselectedLabels: true,
+        landscapeLayout: BottomNavigationBarLandscapeLayout.linear,
+        items: [
+          BottomNavigationBarItem(
+            //backgroundColor: Theme.of(context).primaryColorDark,
+            icon: const Icon(
+              Icons.category,
+              //color: Colors.white,
+            ),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            //backgroundColor: Theme.of(context).primaryColorDark,
+            icon: const Icon(
+              Icons.star,
+            ),
+            label: "Favorites",
+          ),
+        ],
+      ),
+    );
+  }
+}
